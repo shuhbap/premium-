88919fbcf90ccda7467d723d6ff23356bc3c78cc
Shuhaib! <127591148+shuhbap@users.noreply.github.com>
2026-05-23 10:36:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
